d6911296f4ffbd9e9c962eac58d77f5f39be3870
ziyagurel <ziyagurel55@hotmail.com>
2021-07-05 11:02:48 +0300

    Create post component for post design

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Grid, CircularProgress} from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -10,15 +11,15 @@ const Posts = () => {
 
     console.log(posts);
     return (
-        <>
-            <h1>Prof. Dr. Erkan Denizsevmez</h1>
-
-            <h3>Dr. Denizsevmez'e göre diş çürükleri denizdeki alglerin okjisensiz solunumundan kaynaklı olarak gerçekleştiği ve bu alglerin temmuz ve ağustos ayında karadenizde çok yoğun şekilde ortaya çıktığını belirtti.</h3>
-            
-            <h3>Halk arasında deniz cürüğü olarak bilinen magnetik çürüme engellemek için karadenizde girilmemesi gerektiğini altını çizerek belirtti.</h3>
-            <Post/>
-            <Post/>
-        </>
+        !posts.length ? <CircularProgress/> : (
+            <Grid className = { classes.container} container alignItems = "stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key = {post._id} item xs={12} sm={6}>
+                        <Post post = {post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
