440ca3bd7ff6556bcbbd74bc9e4afbfc2ead20f4
ziyagurel <ziyagurel55@hotmail.com>
2021-07-07 13:21:10 +0300

    added clear metod from submit and clear button

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -25,10 +25,12 @@ const Form = ({currentId, setCurrentId}) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({creator: '', title: '', message:'', tags: '', selectedFile: ''})
     }
     
     return (
