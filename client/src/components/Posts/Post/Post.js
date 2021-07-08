2e4bcacdc696f85e6a030a471ac5e0d51926cd83
ziyagurel <ziyagurel55@hotmail.com>
2021-07-08 11:17:37 +0300

    liking is completed

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,12 +7,12 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
 import useStyles from './styles';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({post, setCurrentId}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
-    
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -31,7 +31,7 @@ const Post = ({post, setCurrentId}) => {
                 <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}} > <ThumbUpAltIcon fontSize="small"/> Like {post.likeCount} </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))} > <ThumbUpAltIcon fontSize="small"/> Like {post.likeCount} </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id ))} > <DeleteIcon fontSize="small"/> Delete </Button>
             </CardActions>
         </Card>
