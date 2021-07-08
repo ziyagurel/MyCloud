e1c7710841e45d6689dba6c87438c1404c5ec14e
ziyagurel <ziyagurel55@hotmail.com>
2021-07-09 00:21:14 +0300

    mongo connection text hidden

--- client/src/styles.js
+++ client/src/styles.js
@@ -17,7 +17,7 @@ export default makeStyles((theme) => ({
   },
   [theme.breakpoints.down('sm')] : {
     mainContainer: {
-      flexDirection: "column-reverse"
+      flexDirection: "column-reverse",
     }
   }
 }));
